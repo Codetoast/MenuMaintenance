c6f263c8b3d3b4b9f78eba6199c5452eab297e93
Michael Pelland <michael.pelland@optum.com>
2018-03-07 11:14:45 -0500

    Adding bower and npm dependencies and gulp file

---
+++ ui/gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('build', function () {
+    gulp.src('*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./dist'));
+    gulp.src('*.html')
+        .pipe(gulp.dest('./dist'));
+    //gulp.src('bower_components/**/*')
+    //    .pipe(gulp.dest('./dist/bower_components/'));
+})

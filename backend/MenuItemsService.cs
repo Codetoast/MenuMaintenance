e79e0ec8968bf8bca407bfc374644ab6ace5cb90
Michael Pelland <michael.pelland@optum.com>
2018-03-14 11:09:24 -0400

    Adding new project for azure function

---
+++ backend/MenuItemsService.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace backend
+{
+    public static class MenuItemsService
+    {
+        [FunctionName("MenuItemsService")]
+        public static async Task<IEnumerable<string>> Get([HttpTrigger(AuthorizationLevel.Function, "get", Route = "MenuItems")]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Verbose("Requesting Menu Items.");
+
+            return new string[] { "Michael Burger", "Fries", "Soda" };
+        }
+    }
+}
